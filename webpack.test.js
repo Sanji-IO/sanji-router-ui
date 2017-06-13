fc8aa19262843c6fedf1d2146c5a317e3eed7ddd
Zack Yang <zack9433@gmail.com>
2017-06-13 14:45:36 +0800

    fix(bug): modify ui router repo and fix test

--- webpack.test.js
+++ webpack.test.js
@@ -3,7 +3,9 @@ const config = require('./webpack.config');
 config.devtool = 'source-map';
 // Should be an empty object if it's generating a test build
 // Karma will set this when it's a test build
-config.entry = {};
+config.entry = {
+  main: './app.js'
+};
 // Should be an empty object if it's generating a test build
 // Karma will handle setting it up for you when it's a test build
 config.output = {};
